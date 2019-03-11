edbb6b7b05045ebe61e3d69f17f5d47dc99117cd
alvarviktor <viktor.alvar@yahoo.com>
2019-03-10 19:05:50 -0700

    added location services

--- client/app/src/main/java/c4985/androidgps/MainActivity.java
+++ client/app/src/main/java/c4985/androidgps/MainActivity.java
@@ -19,15 +19,80 @@
 
 package c4985.androidgps;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @SuppressLint("MissingPermission")
+    public void initLocationHandler() {
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final LocationListener locationListener = new MyLocationListener(this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 500, locationListener);
+    }
+
+    public void promptLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            initLocationHandler();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(this, "Location permission is needed to show location.", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        promptLocationPermissions();
+        findViewById(R.id.getLocationButton).setOnClickListener(this);
+        findViewById(R.id.connectButton).setOnClickListener(this);
+        findViewById(R.id.updateButton).setOnClickListener(this);
+        findViewById(R.id.disconnectButton).setOnClickListener(this);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initLocationHandler();
+            } else {
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.getLocationButton:
+                promptLocationPermissions();
+                break;
+
+            case R.id.connectButton:
+                break;
+
+            case R.id.updateButton:
+                break;
+
+            case R.id.disconnectButton:
+                break;
+        }
+    }
 }
---
+++ client/app/src/main/java/c4985/androidgps/MyLocationListener.java
@@ -0,0 +1,42 @@
+package c4985.androidgps;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MyLocationListener implements LocationListener {
+
+    public Activity activity;
+
+    public MyLocationListener(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView latTextView = this.activity.findViewById(R.id.latValue);
+        TextView lngTextView = this.activity.findViewById(R.id.lngValue);
+        latTextView.setText(String.valueOf(new DecimalFormat("###0.00").format(location.getLatitude())));
+        lngTextView.setText(String.valueOf(new DecimalFormat("###0.00").format(location.getLongitude())));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
